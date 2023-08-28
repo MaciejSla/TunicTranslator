6a1a0ec8469f314944191d298d2d7c6b2fced3ea
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-08-28 21:26:28 +0200

    Added character building and pushing to stack (needs an overhaul tbh)

---
+++ src/merged.svelte
@@ -0,0 +1,30 @@
+<svg
+	width="120mm"
+	height="150mm"
+	viewBox="0 0 120 150"
+	version="1.1"
+	id="svg1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:svg="http://www.w3.org/2000/svg"
+>
+	<defs id="defs1" />
+	<g id="layer1">
+		<path
+			style="display:inline;fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.237;stroke-linecap:round;stroke-dasharray:none;stroke-opacity:1"
+			id="rect3"
+			width="100"
+			height="4"
+			x="10"
+			y="73"
+			transform="matrix(1.04,0,0,1,-2.4000003,0)"
+			d="m 12,73 96,0 a 2,2 45 0 1 2,2 2,2 135 0 1 -2,2 H 12 a 2,2 45 0 1 -2,-2 2,2 135 0 1 2,-2 z"
+		/>
+		<circle
+			style="display:inline;fill:none;fill-opacity:1;stroke:#000000;stroke-width:4;stroke-dasharray:none;stroke-opacity:1"
+			id="path7"
+			cx="60"
+			cy="130"
+			r="8"
+		/>
+	</g>
+</svg>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,165 @@
+<script>
+	import list from './elements.json';
+	let store = list.reduce(
+		(prev, curr) => {
+			return { ...prev, [curr.id]: { clicked: false, id: [curr.id], d: [curr.d] } };
+		},
+		{ accent_circle: { clicked: false, id: 'accent_circle' } }
+	);
+	// let overlay = [];
+	let stack = [];
+	let gray = {
+		display: 'inline',
+		fill: 'none',
+		'fill-opacity': 1,
+		'stroke-width': 4,
+		'stroke-linecap': 'round',
+		'stroke-linejoin': 'bevel',
+		'stroke-miterlimit': 1,
+		'stroke-dasharray': 'none',
+		'stroke-dashoffset': 0,
+		'stroke-opacity': 1,
+		'paint-order': 'normal'
+	};
+	function clickme(e) {
+		const id = e.target.id;
+		const clicked = store[id].clicked;
+		store[id].clicked = !clicked;
+		// if (!clicked) {
+		// 	overlay = [...overlay, { id: e.target.id, d: e.target.d }];
+		// } else {
+		// 	overlay = overlay.map((x) => {
+		// 		if (x.id != id) return x;
+		// 	});
+		// }
+		// console.log(overlay);
+	}
+
+	function reset() {
+		Object.keys(store).map((x) => {
+			store[x].clicked = false;
+		});
+	}
+
+	function submit() {
+		const arr = [];
+		Object.keys(store).map((x) => {
+			if (store[x].clicked) {
+				arr.push(x);
+			}
+		});
+		console.log(stack);
+		arr.length != 0 ? (stack = [...stack, arr]) : null;
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div display="flex">
+	{#each stack as elem}
+		<svg
+			width="12mm"
+			height="15mm"
+			viewBox="0 0 120 150"
+			version="1.1"
+			id="svg1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:svg="http://www.w3.org/2000/svg"
+		>
+			{#each elem as id}
+				{#if id == 'accent_circle'}
+					<circle
+						{...gray}
+						fill="#000000"
+						fill-opacity="0"
+						{id}
+						stroke="#000000"
+						cx="60"
+						cy="130"
+						r="8"
+					/>
+				{:else}
+					<path {...gray} d={store[id].d} stroke="#000000" />
+				{/if}
+			{/each}
+			<path
+				style="display:inline;fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.237;stroke-linecap:round;stroke-dasharray:none;stroke-opacity:1"
+				id="base"
+				width="100"
+				height="10"
+				x="10"
+				y="73"
+				transform="matrix(1.04,0,0,1,-2.4000003,0)"
+				d="m 12,73 96,0 a 2,2 45 0 1 2,2 2,2 135 0 1 -2,2 H 12 a 2,2 45 0 1 -2,-2 2,2 135 0 1 2,-2 z"
+			/>
+		</svg>
+	{/each}
+</div>
+
+<svg
+	width="120mm"
+	height="150mm"
+	viewBox="0 0 120 150"
+	version="1.1"
+	id="svg1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:svg="http://www.w3.org/2000/svg"
+>
+	<g>
+		{#each list as elem}
+			<path
+				on:click={clickme}
+				{...gray}
+				{...elem}
+				stroke={store[elem.id].clicked ? '#000000' : '#b4b4b4'}
+			/>
+		{/each}
+		<path
+			style="display:inline;fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.237;stroke-linecap:round;stroke-dasharray:none;stroke-opacity:1"
+			id="base"
+			width="100"
+			height="4"
+			x="10"
+			y="73"
+			transform="matrix(1.04,0,0,1,-2.4000003,0)"
+			d="m 12,73 96,0 a 2,2 45 0 1 2,2 2,2 135 0 1 -2,2 H 12 a 2,2 45 0 1 -2,-2 2,2 135 0 1 2,-2 z"
+		/>
+		<circle
+			on:click={clickme}
+			{...gray}
+			fill="#000000"
+			fill-opacity="0"
+			id="accent_circle"
+			stroke={store['accent_circle'].clicked ? '#000000' : '#b4b4b4'}
+			cx="60"
+			cy="130"
+			r="8"
+		/>
+	</g>
+	<!-- ! Figure this out -->
+	<!-- <g>
+		{#each overlay as elem}
+			{#if elem.id == 'accent_circle'}
+				<circle
+					{...gray}
+					fill="#000000"
+					fill-opacity="0"
+					id={elem.id}
+					stroke="#000000"
+					cx="60"
+					cy="130"
+					r="8"
+				/>
+			{:else}
+				<path on:click={clickme} {...gray} {...elem} stroke="#000000" />
+			{/if}
+		{/each}
+	</g> -->
+</svg>
+
+<button on:click={reset}>Reset</button>
+<button on:click={submit}>Submit</button>
+
+<style>
+</style>
